a99fae0a87fa7b94e364aaeb48a99b4f33d0d258
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-01-22 10:24:36 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputValue = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
-
+text.style.fontSize = `${Number(inputValue.value)}px`;
 inputValue.addEventListener('input', () => { 
     text.style.fontSize = `${Number(inputValue.value)}px`;
 })
